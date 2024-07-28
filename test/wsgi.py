c225c073ad484cbbfc2f2996682f790ad048d563
pepseada <m.seada2002@gmail.com>
2024-07-29 02:27:52 +0300

    Add

--- test/wsgi.py
+++ test/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for test project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Path to your Django project directory
+path = '/home/seada/test-pythonanywhere'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Path to the directory containing the settings module
+sys.path.append('/home/seada/test-pythonanywhere/test')
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test.settings'
+
+# Import and set up the WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test.settings')
-
 application = get_wsgi_application()
